4be0dda8139960b40586492571263cfe25acfcda
Divine-Bi <dbingila@outlook.com>
2026-06-23 10:12:47 +0200

    fix: browserouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from "./context/AuthContext";
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter basename="/shiftm2-frontend">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
