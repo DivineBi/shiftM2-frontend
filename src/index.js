4a48f6779af7ac608a97b9a511079180dc03ba5f
Divine-Bi <dbingila@outlook.com>
2026-06-24 07:02:14 +0200

    fix: routing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/shiftM2-frontend">
       <AuthProvider>
         <App />
       </AuthProvider>
